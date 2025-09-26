9bcb4b20acc16b10768a01ccb78b27b1d2cd2acc
uriapete <uriarteangelo@gmail.com>
2025-09-25 17:26:15 -0700

    touch LinkedList class

---
+++ src/linkedList/LinkedList.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package linkedList;
+
+/**
+ * 
+ */
+public class LinkedList {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
