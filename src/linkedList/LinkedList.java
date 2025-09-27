b6d9f8b516cec9c30e6e34e6e13010e23977e337
uriapete <uriarteangelo@gmail.com>
2025-09-27 15:13:10 -0700

    LL - getRoot and traverse fns

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -2,12 +2,22 @@
  * 
  */
 package linkedList;
-
+import java.util.function.Consumer;
 /**
  * 
  */
 public class LinkedList<T> {
 	private LinkedListNode<T> root;
+
+	public LinkedListNode<T> getRoot(){
+		return root;
+	}
+	
+	public void traverse(Consumer<LinkedListNode<T>> fn) {
+		for(LinkedListNode<T> currNode = root; currNode!=null; currNode = currNode.getNext()) {
+			fn.accept(currNode);
+		}
+	}
 
 	/**
 	 * @param args
