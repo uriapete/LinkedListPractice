f37da8122781980b622c7e4de3981deb300e4e87
uriapete <uriarteangelo@gmail.com>
2025-09-27 15:23:10 -0700

    comment root getter and traverse function

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -9,10 +9,12 @@ import java.util.function.Consumer;
 public class LinkedList<T> {
 	private LinkedListNode<T> root;
 
+//	root getter
 	public LinkedListNode<T> getRoot(){
 		return root;
 	}
 	
+//	foreach function for this list
 	public void traverse(Consumer<LinkedListNode<T>> fn) {
 		for(LinkedListNode<T> currNode = root; currNode!=null; currNode = currNode.getNext()) {
 			fn.accept(currNode);
