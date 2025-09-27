9f6e9b7e7e295bf8bad21b14b61a6ddcdbd50898
uriapete <uriarteangelo@gmail.com>
2025-09-27 14:53:52 -0700

    LLN constructors

--- src/linkedList/LinkedListNode.java
+++ src/linkedList/LinkedListNode.java
@@ -11,4 +11,15 @@ public class LinkedListNode<T> {
 	
 //	ref to next node
 	LinkedListNode<T> next;
+	
+//	base constructor
+	public LinkedListNode(T d){
+		data = d;
+	}
+	
+// 	constructor with next option
+	public LinkedListNode(T d, LinkedListNode<T> n) {
+		new LinkedListNode<T>(d);
+		next = n;
+	}
 }
