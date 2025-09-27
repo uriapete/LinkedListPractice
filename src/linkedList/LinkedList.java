9a094cf7e80238d5ed96ca396f7f47bdddd9ecdb
uriapete <uriarteangelo@gmail.com>
2025-09-27 15:31:20 -0700

    LL new field - tail

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -14,6 +14,14 @@ public class LinkedList<T> {
 		return head;
 	}
 	
+//	last item
+	private LinkedListNode<T> tail;
+	
+//	tail getter
+	public LinkedListNode<T> getTail(){
+		return tail;
+	}
+	
 //	foreach function for this list
 	public void traverse(Consumer<LinkedListNode<T>> fn) {
 		for(LinkedListNode<T> currNode = head; currNode!=null; currNode = currNode.getNext()) {
